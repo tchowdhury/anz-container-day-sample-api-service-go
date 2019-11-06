47469e49971b44e470aff06750b8ebd5054db793
TC <tchowdhury@users.noreply.github.com>
2019-11-06 13:50:58 +1100

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello TC's World"}
+		res := &response{Message: "Hello TCs World"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
